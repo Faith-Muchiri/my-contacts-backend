18022443571e2ed09b57af17779a0b944d99d95f
Faith <faithwambui891@gmail.com>
2024-01-16 10:18:50 +0300

    my contacts nodejs backend

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const connectDB = require('./config/dbConnection')
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
+connectDB();
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/api/v1/contacts', require('./routes/contactRoutes'));
+app.use('/api/v1/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
